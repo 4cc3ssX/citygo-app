451437a5051b51d87acbc03d6e112031550de842
4cc3ssX <4cc3ssflick@gmail.com>
2023-12-11 23:31:11 +0630

    refactor: add doc strings to db model

--- src/typescript/models/routes.ts
+++ src/typescript/models/routes.ts
@@ -2,11 +2,29 @@ import { ICoordinates, ILocalizedString } from ".";
 import { ReplaceValueByType } from "..";
 
 export interface IRoute {
+  /**
+   * The ID of the bus agency.
+   */
   agency_id: string;
+  /**
+   * The gate name of the bus.
+   */
   name: ILocalizedString;
+  /**
+   * The color of the bus line.
+   */
   color: string;
+  /**
+   * The ID of the bus line.
+   */
   route_id: string;
+  /**
+   * The IDs of the bus stop.
+   */
   stop: number[];
+  /**
+   * The LineString coordinates of the bus stop.
+   */
   coordinates: ICoordinates[];
 }
 
--- src/typescript/models/stops.ts
+++ src/typescript/models/stops.ts
@@ -2,9 +2,21 @@ import { ICoordinates, ILocalizedString } from ".";
 import { ReplaceValueByType } from "..";
 
 export interface IStop extends ICoordinates {
+  /**
+   * The ID of the stop
+   */
   id: number;
+  /**
+   * The name of the stop
+   */
   name: ILocalizedString;
+  /**
+   * The road name of the stop
+   */
   road: ILocalizedString;
+  /**
+   * The township name of the stop
+   */
   township: ILocalizedString;
 }
 
