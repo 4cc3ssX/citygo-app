e361a270e3b53e665ca45c32980d03d33ee803dc
4cc3ssX <4cc3ssflick@gmail.com>
2024-03-06 23:20:17 +0630

    fix: remove file logging

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,31 +1,8 @@
 import { createLogger, transports, format, LoggerOptions } from "winston";
-import "winston-daily-rotate-file";
-
-const infoTransport = new transports.DailyRotateFile({
-  level: "info",
-  dirname: "logs",
-  filename: "onebus_info_%DATE%.log",
-  datePattern: "DD-MM-YYYY",
-  maxSize: "50m",
-  maxFiles: "14d",
-});
-
-const errorTransport = new transports.DailyRotateFile({
-  level: "error",
-  dirname: "logs",
-  filename: "onebus_error_%DATE%.log",
-  datePattern: "DD-MM-YYYY",
-  maxSize: "50m",
-  maxFiles: "14d",
-});
 
 const logConfiguration: LoggerOptions = {
   level: "info",
   format: format.json(),
-  transports:
-    process.env.NODE_ENV === "production"
-      ? [infoTransport, errorTransport]
-      : undefined,
 };
 
 const customFormat = format.printf((info) => {
